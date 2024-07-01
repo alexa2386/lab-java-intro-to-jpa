e3ef58d84faf56be2755cc4dc6e676c30a3a5e0e
Alexia <alexa2386@yahoo.com>
2024-07-01 19:29:49 -0400

    Update README.md

---
+++ src/main/java/com/example/lab_java_intro_to_jpa/model/FlightBooking.java
@@ -0,0 +1,52 @@
+package com.example.lab_java_intro_to_jpa.model;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class FlightBooking {
+
+    private Integer bookingId;
+    private Integer customerId;
+    private Integer flightId;
+
+    public FlightBooking() {
+    }
+
+    public FlightBooking(Integer customerId, Integer flightId) {
+        this.customerId = customerId;
+        this.flightId = flightId;
+    }
+
+    public Integer getBookingId() {
+        return bookingId;
+    }
+
+    public void setBookingId(Integer bookingId) {
+        this.bookingId = bookingId;
+    }
+
+    public Integer getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Integer customerId) {
+        this.customerId = customerId;
+    }
+
+    public Integer getFlightId() {
+        return flightId;
+    }
+
+    public void setFlightId(Integer flightId) {
+        this.flightId = flightId;
+    }
+
+    @Override
+    public String toString() {
+        return "FlightBooking{" +
+                "bookingId=" + bookingId +
+                ", customerId=" + customerId +
+                ", flightId=" + flightId +
+                '}';
+    }
+}
